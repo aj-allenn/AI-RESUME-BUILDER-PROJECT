9a57e1212577b5543cad82312f54e7fe248b4cc9
aj-allenn <johnallenjohn2004@gmail.com>
2026-02-10 18:01:24 +0530

    personal info form

--- client/src/pages/Builder.jsx
+++ client/src/pages/Builder.jsx
@@ -1,10 +1,118 @@
 
+import { useState } from "react";
 import Navbar from "../components/home/Navbar";
+import { ArrowLeftIcon,Briefcase,FolderIcon,GraduationCap,User,FileText,Sparkles, ChevronLeft, ChevronRight } from "lucide-react";
+import { Link } from "react-router-dom";
+import PersonalInfoForm from "../components/PersonalInfoForm";
 
 const Builder = () => {
+
+   const [resumeData,setResumeData]=useState(
+    {
+      _id:'',
+      title:'',
+      personal_info:{},
+      professional_summmary:'',
+      experience:[],
+      education:[],
+      skills:[],
+      public:false,
+      accent_color:'#03879'
+
+  }
+   );
+
+   const [activeSectionIndex, setActiveSectionIndex]= useState(0)
+   const [removeBackground,setRemoveBackground]=useState(false);
+
+   const sections=[
+    {id:"personal",name:"Personal Info", icon:User },
+    {id:"summary",name:"Summary", icon:FileText },
+    {id:"experience",name:"Experience", icon:Briefcase },
+    {id:"education",name:"Education", icon:GraduationCap },
+    {id:"projects",name:"Projects", icon:FolderIcon },
+    {id:"skills",name:"Skills", icon:Sparkles },
+   ]
+
+
+   const activeSection = sections[activeSectionIndex]
+
+
   return (
    <div>
-    <h2>builder page</h2>
+   
+     <div className="max-w-7xl mx-auto px-2 py-2">
+      <Link to={'/app'} className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all">
+      <ArrowLeftIcon className="size-4"/>
+         Back to Dashboard
+      </Link>
+     </div>
+
+
+
+
+     <div className="max-w-7xl mx-auto px-4 pb-8">
+      <div className="grid lg:grid-cols-12 gap-8">
+
+        {/* left panel */}
+
+        <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
+          <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1 ">
+
+            {/* progress bar using activeSectionIndex */}
+            <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
+
+            <hr className="absolute top-0 left-0 h-1 bg-gradient-to-r from-indigo-500 to-violet-600 border-none transition-all duration-2000"
+            style={{width:`${activeSectionIndex * 100 / (sections.length -1 )}
+            % `}}/>
+
+            {/* section nav */}
+
+            <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+              {/* left-left */}
+              <div></div>
+
+
+
+
+
+              {/* left-right */}
+              <div className="flex items-center">
+                {activeSectionIndex !== 0 &&(
+                  <button onClick={()=>setActiveSectionIndex((prevIndex)=>Math.max(prevIndex-1 ,0))} className="flex item-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600
+                   hover:bg-gray-50 transition-all" disabled={activeSectionIndex === 0 }>
+                    <ChevronLeft className="size-4"/> Previous
+                  </button>
+                )}
+
+                 <button onClick={()=>setActiveSectionIndex((prevIndex)=>Math.min(prevIndex + 1 ,sections.length -1))} className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length-1 && 'opacity-50'}`} disabled={activeSectionIndex === sections.length-1 }>
+                    Next
+                    <ChevronRight className="size-4"/> 
+                  </button>
+              </div>
+
+            </div>
+           {/* form content */}
+
+            <div className="space-y-6">
+              {activeSection.id === 'personal' && (
+                <PersonalInfoForm data={resumeData.personal_info} onChange={(data)=>setResumeData(prev => ({...prev, personal_info:data}))} removeBackground={removeBackground}
+                setRemoveBackground={setRemoveBackground}/>
+              )}
+
+            </div>
+
+          </div>
+           
+        </div>
+
+        {/* right */}
+        <div></div>
+
+      </div>
+
+     </div>
+
    </div>
     
   )
